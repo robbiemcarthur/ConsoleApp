f6b59b30ec11055e237689e4d5ce3065e4c28a42
Robbie McArthur <2098323m@student.gla.ac.uk>
2018-06-04 00:06:30 +0100

    update

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,12 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * Basic 'Hello World' console application to demonstrate C# understanding
+ */
+
 namespace ConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Please enter a message to display.");
+            var input = Console.ReadLine();
+            Console.WriteLine("Displaying message: {0}", input);
+            Console.WriteLine("Press enter to exit.");
+            input = Console.ReadLine();
         }
     }
 }
